554eb35716965379c9286ab7f2aa4f9560998fbc
juniorh28 <juniorh2897@gmail.com>
2022-06-03 15:19:27 -0400

    lc286: cyclicSorted then for loop to find missing num

--- Assignments/cyclicSort/cyclicSortEasy.js
+++ Assignments/cyclicSort/cyclicSortEasy.js
@@ -1,11 +1,23 @@
 // Problem Statement #
 
-// We are given an array containing ‘n’ objects. Each object, when created, was assigned a unique number from 1 to ‘n’ based on their creation sequence. This means that the object with sequence number ‘3’ was created just before the object with sequence number ‘4’.
+// We are given an array containing ‘n’ objects. 
+//Each object, when created, was assigned a unique number from 1 to ‘n’ based on their creation sequence. 
+//This means that the object with sequence number ‘3’ was created just before the object with sequence number ‘4’.
 
-// Write a function to sort the objects in-place on their creation sequence number in O(n) and without any extra space. For simplicity, let’s assume we are passed an integer array containing only the sequence numbers, though each number is actually an object.
+// Write a function to sort the objects in-place on their creation sequence number in O(n) and without any extra space. 
+//For simplicity, let’s assume we are passed an integer array containing only the sequence numbers, though each number is actually an object.
 
 const cyclic_sort = function (nums) {
-  // TODO: Write your code here
+  let i=0;
+  while(i < nums.length){
+    let j = nums[i] - 1;
+    if(nums[i] != nums[j]){
+      [nums[i], nums[j]] = [nums[j], nums[i]]
+    }
+    else
+      ++i;
+  }
+  console.log(nums)
   return nums;
 };
 
--- Assignments/cyclicSort/lc268.js
+++ Assignments/cyclicSort/lc268.js
@@ -1,10 +1,26 @@
 // Problem Statement #
 
-// We are given an array containing ‘n’ distinct numbers taken from the range 0 to ‘n’. Since the array has only ‘n’ numbers out of the total ‘n+1’ numbers, find the missing number.
+// We are given an array containing ‘n’ distinct numbers taken from the range 0 to ‘n’. 
+// Since the array has only ‘n’ numbers out of the total ‘n+1’ numbers, find the missing number.
 
 const find_missing_number = function(nums) {
   // TODO: Write your code here
-  return -1;
+  let searchFor=0;
+  let i=0;
+  while(i < nums.length){
+    let j = nums[i] - 1;
+    if(nums[i] != nums[j]){
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+      else
+        ++i;
+  }
+
+  for(let j= -1; j<nums.length; ++j){
+    if(nums[j] != j+1)
+      searchFor = j+1;
+  }
+  return searchFor;
 };
 
 console.log(find_missing_number([4, 0, 3, 1]));
