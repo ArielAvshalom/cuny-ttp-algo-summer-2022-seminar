0ed37c81726d4c99a77e50d102c1914119f97ecf
Ishmam Haque <59802447+IshmamHaque1112@users.noreply.github.com>
2022-06-04 17:23:22 -0400

    My interview answer combined with the correct answer

--- Students/ishmamHaque/week_1/day_2/interviewanswer.js
+++ Students/ishmamHaque/week_1/day_2/interviewanswer.js
@@ -1,1 +1,34 @@
+const find_all_duplicates = function(nums) {
+  duplicateNumbers = [];
+  let i=0;
+  //This is my sorting alg
+  while(i<nums.length){
+   const j= nums[i]-1;
+   if(nums[i]!==nums[j]){
+    let temp=nums[i];
+    nums[i]=nums[j];
+    nums[j]=temp;
+   }
+    else{
+     i=i+1;
+    }
+  }
+  console.log(nums);
+ // This is my interview solution loop which was wrong because it does not sort it as your average sorting algorithm does
+ // for(a=1;a<nums.length;a++){
+ //  if(nums[i]==nums[i-1]){
+ //   duplicateNumbers.push(nums[i])
+ //  }
+ // }
+ //this is the correct answer.
+ for (i = 0; i < nums.length; i++) {
+   if (nums[i] !== i + 1) {
+     duplicateNumbers.push(nums[i]);
+   }
+ }
+  return duplicateNumbers;
+};
+
+console.log(find_all_duplicates([3, 4, 4, 5, 5]));
+console.log(find_all_duplicates([5, 4, 7, 2, 3, 5, 3]));
 
