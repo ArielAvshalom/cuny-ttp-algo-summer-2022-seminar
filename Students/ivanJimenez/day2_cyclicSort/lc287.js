f4da59d389e0479a4e04e442c5adaa4c6931edc4
IvanJimenezCodeHub <jimenezivan338@gmail.com>
2022-06-05 21:02:26 -0400

    Complete the lc287 problem

--- Students/ivanJimenez/day2_cyclicSort/lc287.js
+++ Students/ivanJimenez/day2_cyclicSort/lc287.js
@@ -3,7 +3,23 @@
 // We are given an unsorted array containing ‘n+1’ numbers taken from the range 1 to ‘n’. The array has only one duplicate but it can be repeated multiple times. Find that duplicate number without using any extra space. You are, however, allowed to modify the input array.
 
 const find_duplicate = function(nums) {
-  // TODO: Write your code here
+  let i = 0;
+  while(i < nums.length){
+    let j = nums[i] - 1;
+    if(nums[i] !== nums[j]){
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+    else{
+      i++;
+    }
+  }
+
+  for(let k = 0; k < nums.length; k++){
+    if(nums[k] - 1 !== k){
+      return nums[k];
+    }
+  }
+  // This statement is never reached because the duplicate will always be found in the for loop
   return -1;
 };
 
