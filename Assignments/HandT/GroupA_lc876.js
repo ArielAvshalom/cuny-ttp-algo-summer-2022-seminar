782cbff640999b80cc83f9c14ad29e7b3db57fcd
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2022-06-07 12:02:58 -0400

    Add 876

--- Assignments/HandT/GroupA_lc876.js
+++ Assignments/HandT/GroupA_lc876.js
@@ -32,3 +32,23 @@ head.next.next.next.next.next.next = new Node(7)
 console.log(`Middle Node: ${find_middle_of_linked_list(head).value}`)
 
 
+
+// Solution
+// -----
+// function find_middle_of_linked_list(head) {
+//   let slow = head,
+//     fast = head;
+//   while ((fast !== null && fast.next !== null)) {
+//     slow = slow.next;
+//     fast = fast.next.next;
+//   }
+//   return slow;
+// }
+
+// -----
+
+// Time complexity #
+// The above algorithm will have a time complexity of O(N) where ‘N’ is the number of nodes in the LinkedList.
+
+// Space complexity #
+// The algorithm runs in constant space O(1).
