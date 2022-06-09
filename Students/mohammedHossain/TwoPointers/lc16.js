e4daf478a2cf6e62e097cbe64b07e413f7a01d23
jahedev <jahedev@gmail.com>
2022-06-09 17:29:30 -0400

    mh: reverse linked list

---
+++ Students/mohammedHossain/TwoPointers/lc16.js
@@ -0,0 +1,12 @@
+// Problem Statement #
+// Given an array of unsorted numbers and a target number, find a triplet in the array whose sum is as close to the target number as possible, return the sum of the triplet. If there are more than one such triplet, return the sum of the triplet with the smallest sum.
+
+const triplet_sum_close_to_target = function (arr, target_sum) {
+  // TODO: Write your code here
+  return -1;
+};
+
+console.log(triplet_sum_close_to_target([-2, 0, 1, 2], 2));
+console.log(triplet_sum_close_to_target([-3, -1, 1, 2], 1));
+console.log(triplet_sum_close_to_target([1, 0, 1, 1], 100));
+
