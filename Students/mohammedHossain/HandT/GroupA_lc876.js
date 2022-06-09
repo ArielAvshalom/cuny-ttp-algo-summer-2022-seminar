8bd96f4e8db6f359faebd2727408e1e605e7bc76
jahedev <jahedev@gmail.com>
2022-06-09 18:22:39 -0400

    mh: lc876

--- Students/mohammedHossain/HandT/GroupA_lc876.js
+++ Students/mohammedHossain/HandT/GroupA_lc876.js
@@ -5,17 +5,34 @@
 // If the total number of nodes in the LinkedList is even, return the second middle node.
 
 class Node {
-  constructor(value, next=null){
-    this.value = value;
-    this.next = next;
+  constructor(value, next = null) {
+    this.value = value
+    this.next = next
   }
 }
 
-const find_middle_of_linked_list = function(head) {
-  // TODO: Write your code here
-  return head;
-}
+const find_middle_of_linked_list = function (head) {
+  let slow = head
+  let fast = head
 
+  // since fast will be 2 times fast, buy the end, slow will be in the middle
+  // e.g. 1 -> 2 -> 3 -> 4 -> 5
+  //     ^^fs
+  //           ^s   ^f
+  //                ^s        ^f
+
+  // case: even nodes
+  // e.g. 1 -> 2 -> 3 -> 4
+  //      ^^fs
+  //           ^s   ^f
+  //                ^s   ^f
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next
+    fast = fast.next.next
+  }
+
+  return slow
+}
 
 head = new Node(1)
 head.next = new Node(2)
@@ -30,8 +47,6 @@ console.log(`Middle Node: ${find_middle_of_linked_list(head).value}`)
 
 head.next.next.next.next.next.next = new Node(7)
 console.log(`Middle Node: ${find_middle_of_linked_list(head).value}`)
-
-
 
 // Solution
 // -----
