7bb41fa6829f3cebcdf10de94f6d44e807091aea
Binqing Zheng <bqz2101@gmail.com>
2022-06-11 01:48:39 -0400

    Submitted Merge_Interval problem # lc986
    Submitted  Merge_Interval problem #lc986. TypeError with print_interval().

--- Students/BinQingZheng/Week_1_HW/Merge_Intervals/lc986.js
+++ Students/BinQingZheng/Week_1_HW/Merge_Intervals/lc986.js
@@ -1,6 +1,7 @@
 // Problem Statement #
 
-// Given two lists of intervals, find the intersection of these two lists. Each list consists of disjoint intervals sorted on their start time.
+// Given two lists of intervals, find the intersection of these two lists. 
+// Each list consists of disjoint intervals sorted on their start time.
 
 class Interval {
     constructor(start, end) {
@@ -8,14 +9,32 @@ class Interval {
       this.end = end;
     }
   
-    print_interval() {
+    print_interval () {
       process.stdout.write(`[${this.start}, ${this.end}]`);
     }
-  }
+}
   
-  const merge = function(intervals_a, intervals_b) {
+const merge = function(intervals_a, intervals_b) {
     let result = [];
-    // TODO: Write your code here
+    if (intervals_a.length == 0 || intervals_b.length == 0) {
+        return result;
+    }
+
+    let a = 0, // pointer for intervlas_a 
+        b = 0; //pointer for intervals_b
+    while (a < intervals_a.length && b < intervals_b.length) {
+        if (intervals_b[b].start <= intervals_a[a].end && intervals_a[a].start <= intervals_b[b].end ) {
+            const maxStart = Math.max(intervals_a[a].start, intervals_b[b].start);
+            const minEnd = Math.min(intervals_a[a].end, intervals_b[b].end);
+            result.push([maxStart, minEnd]);
+        }
+        // increment a, b pointers depending on which end point of the two intervals is smaller
+        if(intervals_a[a].end < intervals_b[b].end) {
+            a++
+        } else {
+            b++
+        }
+    }
     return result;
   };
   
@@ -26,9 +45,13 @@ class Interval {
   }
   console.log();
   
+  
   process.stdout.write('Intervals Intersection: ');
   result = merge([new Interval(1, 3), new Interval(5, 7), new Interval(9, 12)], [new Interval(5, 10)]);
   for (i = 0; i < result.length; i++) {
     result[i].print_interval();
   }
   console.log();
+
+  
+
