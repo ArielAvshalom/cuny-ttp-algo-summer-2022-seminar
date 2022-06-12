7d111820090aad40f695f81be7425dd73ac1cf8d
jahedev <jahedev@gmail.com>
2022-06-11 21:05:55 -0400

    cyclic sort problems

--- Students/mohammedHossain/cyclicSort/cyclicSortEasy.js
+++ Students/mohammedHossain/cyclicSort/cyclicSortEasy.js
@@ -34,7 +34,7 @@ function swap(arr, i, j) {
 //   let i=0;
 //   while(i < nums.length){
 //     let j = nums[i] - 1;
-//     if(nums[i] != nums[j]){
+//     if(nums[i] !== nums[j]){
 //       [nums[i], nums[j]] = [nums[j], nums[i]]
 //     }
 //     else
