415a7811a81dac3b1ba3cd3ed828d33f556d50f4
IvanJimenezCodeHub <jimenezivan338@gmail.com>
2022-06-12 19:21:59 -0400

    Completed lc1 problem for day 5 two pointers

--- Students/ivanJimenez/day5_TwoPointers/lc1.js
+++ Students/ivanJimenez/day5_TwoPointers/lc1.js
@@ -4,7 +4,21 @@
 // Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
 
 function pair_with_target_sum(arr, targetSum) {
-  // TODO:write code here
+  let left = 0, right = arr.length - 1;
+  while(left < right){
+    const currentSum = arr[left] + arr[right];
+    if(currentSum === targetSum){
+      return [left, right];
+    }
+
+    if(targetSum > currentSum){
+      left += 1;
+    }
+    else{
+      right -= 1;
+    }
+  }
+  return [-1, -1];
 }
 
 console.log(pair_with_target_sum([1, 2, 3, 4, 6], 6));
