2599cb52bf8028ef504b7cf9495f0c9c9431afe3
juniorh28 <juniorh2897@gmail.com>
2022-06-12 11:42:43 -0400

    lc15 found all arrangement of triplet that =0 but not unique

--- Students/juniorHernandez/week_1/day5/TwoPointers/lc15.js
+++ Students/juniorHernandez/week_1/day5/TwoPointers/lc15.js
@@ -3,20 +3,34 @@
 
 function search_triplets(arr) {
   let triplets = [];
-  arr = arr.sort()
+  // arr = arr.sort()
   //check for any 3 element whose sum equals 0.
   //add those into a triplet array then remove those elements from arr.
   //repeat the check and keep adding triplets into triplet array when found.
   //if no triplet is found return triplet
 
   let size = arr.length;
-  while (true) {
+ for (let pointer1 = 0; pointer1 < arr.length-2; pointer1++) {//only go up to the 3rd to last index
+   for(let pointer2 = 1; pointer2 < arr.length-1; pointer2++){//only go up to 2nd to last index
+     let pointer3 = pointer2 + 1;// pointer3 is alwasy going to be one more than pointer2
+     console.log(`p1: ${pointer1} p2: ${pointer2} p3: ${pointer3} last idx: ${arr.length}`)
+     while(pointer3 < arr.length)//while pointer3 only goes up to the last index
+     {
+       let sum = arr[pointer1] + arr[pointer2] + arr[pointer3];
+       console.log(`sum of ${arr[pointer1]} + ${arr[pointer2]} + ${arr[pointer3]} = ${sum}`)
+       if(arr[pointer1] + arr[pointer2] + arr[pointer3] === 0){ //check if the sum of all element at their idx = 0
+         triplets.push([arr[pointer1], arr[pointer2], arr[pointer3]]);
+         ++pointer3;
+       }else{
+         ++pointer3;
+       }
+     }
+   }
+ }
 
-    break;
-  }
-
+ console.log(triplets)
   return triplets;
 }
 
 console.log(search_triplets([-3, 0, 1, 2, -1, 1, -2]));
-console.log(search_triplets([-5, 2, -1, -2, 3]));
+// console.log(search_triplets([-5, 2, -1, -2, 3]));
