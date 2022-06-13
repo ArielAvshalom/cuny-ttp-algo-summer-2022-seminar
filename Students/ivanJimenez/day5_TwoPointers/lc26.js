305b10d59655f11eaec0df2afc0ba6c48d6cf683
IvanJimenezCodeHub <jimenezivan338@gmail.com>
2022-06-12 21:17:55 -0400

    Completed lc26 problem for day 5 two pointers

--- Students/ivanJimenez/day5_TwoPointers/lc26.js
+++ Students/ivanJimenez/day5_TwoPointers/lc26.js
@@ -2,7 +2,22 @@
 // Given an array of sorted numbers, remove all duplicates from it. You should not use any extra space; after removing the duplicates in-place return the length of the subarray that has no duplicate in it.
 
 function remove_element(arr, key) {
-  // TODO: Write code here
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+
+  let start = 0;
+  let end = arr.length - 1;
+  while(arr[start] !== key || arr[end] !== key){
+    if(arr[start] !== key){
+      start += 1;
+    }
+    if(arr[end] !== key){
+      end -= 1;
+    }
+  }
+  arr.splice(start, end - 1);
+  return arr.length;
 }
 
 console.log(
