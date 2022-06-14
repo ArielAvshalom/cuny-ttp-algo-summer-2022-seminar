cc6974126c0160e62d9b78d1958386d1716313ba
Mohammed J. Hossain <jahedev@gmail.com>
2022-06-14 01:14:37 -0400

    copied questions

---
+++ Students/mohammedHossain/SlidingWindow/lc340.js
@@ -0,0 +1,29 @@
+// Problem Statement #
+
+// Given a string, find the length of the longest substring in it with no more than K distinct characters.
+
+// You can assume that K is less than or equal to the length of the given string.
+
+function longest_substring_with_k_distinct(str, k) {
+  // TODO: Write code here
+}
+
+console.log(
+  `Length of the longest substring: ${longest_substring_with_k_distinct(
+    'araaci',
+    2
+  )}`
+);
+console.log(
+  `Length of the longest substring: ${longest_substring_with_k_distinct(
+    'araaci',
+    1
+  )}`
+);
+console.log(
+  `Length of the longest substring: ${longest_substring_with_k_distinct(
+    'cbbebi',
+    3
+  )}`
+);
+
---
+++ Students/mohammedHossain/SlidingWindow/lc53.js
@@ -0,0 +1,22 @@
+// Problem Statement #
+
+// Given an array of positive numbers and a positive number ‘k,’ find the maximum sum of any contiguous subarray of size ‘k’.
+
+function max_subarray_size_k(k, arr) {
+  // TODO: Write your code here
+}
+
+console.log(
+  `Maximum sum of a subarray of size K: ${max_subarray_size_k(
+    3,
+    [2, 1, 5, 1, 3, 2]
+  )}`
+);
+
+console.log(
+  `Maximum sum of a subarray of size K: ${max_subarray_size_k(
+    2,
+    [2, 3, 4, 1, 5]
+  )}`
+);
+
