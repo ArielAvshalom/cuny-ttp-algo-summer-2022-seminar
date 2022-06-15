04a6898672deeda36918a8adf8bf82505c98e815
Binqing Zheng <bqz2101@gmail.com>
2022-06-15 01:51:44 -0400

    Completed BFS # lc104

---
+++ Students/BinQingZheng/Week_2_HW/BFS/lc104.js
@@ -0,0 +1,58 @@
+// Problem#
+
+// Given a binary tree, find its maximum depth (or height).
+
+class TreeNode {
+    constructor(val) {
+      this.val = val;
+      this.left = null;
+      this.right = null;
+    }
+}
+
+// BFS
+function find_maximum_depth(root) {
+    //If root is null return 0
+    if(!root) return 0
+    
+    let level = 0
+    const queue = [root]
+
+    while (queue.length !== 0) {
+        // Get the length prior to dequeueing, # of nodes must be process in the current tree level 
+        const queueLength = queue.length
+        for (let i = 0; i < queueLength; i++) {
+            //pop the next node
+            const current = queue.shift()
+            
+            if(current.left) {
+                queue.push(current.left)
+            }
+            if(current.right) {
+                queue.push(current.right)
+            }
+        }
+        level += 1
+    }
+    return level
+}
+
+//Recursive DFS 
+function maxium_depth_recursive (root) {
+    // If root is null return 0
+    if (!root) return 0;
+    //else if ((!root.left) && (!root.right)) return 1;
+    else return 1 + Math.max(maxium_depth_recursive(root.left), maxium_depth_recursive(root.right));        
+};
+    
+const root = new TreeNode(12);
+root.left = new TreeNode(7);
+root.right = new TreeNode(1);
+root.right.left = new TreeNode(10);
+root.right.right = new TreeNode(5);
+console.log(`Tree Maximum Depth: ${find_maximum_depth(root)}`);
+console.log(`Tree Maximum Depth - recursive solution : ${find_maximum_depth(root)}`);
+root.left.left = new TreeNode(9);
+root.right.left.left = new TreeNode(11);
+console.log(`Tree Maximum Depth: ${find_maximum_depth(root)}`);
+console.log(`Tree Maximum Depth - recursive solution: ${find_maximum_depth(root)}`);
