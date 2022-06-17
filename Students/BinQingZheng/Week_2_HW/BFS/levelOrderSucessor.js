0877a08958d68f780694f6de55fb61c8af2f596a
Binqing Zheng <bqz2101@gmail.com>
2022-06-17 15:44:17 -0400

    Updated BFS levelOrderSucessor

--- Students/BinQingZheng/Week_2_HW/BFS/levelOrderSucessor.js
+++ Students/BinQingZheng/Week_2_HW/BFS/levelOrderSucessor.js
@@ -43,7 +43,8 @@ function treeNode(value)
 const find_successor = function(root, key) {
     if (!root) return null
 
-    if (root === key) {
+    if (root.val === key) {
+    //if (root === key) {
         
         if (root.left !== null) {
             return root.left
@@ -57,34 +58,48 @@ const find_successor = function(root, key) {
         }
     }
     const queue = [root];
-    console.log("root: " + root)
+    console.log("root: " + root.val)
 
-    while (queue.length  !== 0) {
+    while (queue.length  > 0) {
         //const queueLength = queue.length // get the queue length before dequeueing
         //for (let i = 0; i < queueLength; i++) {
             const current_node  = queue[0]
             console.log("current node: " + current_node.val)
             let shiftedNode = queue.shift();
             console.log("shiftedNode : " + shiftedNode.val)
+            console.log("current node after shifted node: " + current_node.val)
             
             if (current_node.left) {
-                console.log ("current node's left child " + current_node.left)
-                queue.push(current_node.left)
+                let leftchild = current_node.left
+                console.log ("current node's left child " + leftchild.val)
+                console.log ("new length after adding leftchild " + queue.push(current_node.left))
             }
             if(current_node.right) {
-                console.log ("current node's right child " + current_node.right)
-                queue.push(current_node.right)
+                let rightchild = current_node.right
+                console.log ("current node's right child " + rightchild.val)
+                console.log (" new length after adding rightchild: " + queue.push(current_node.right))
             }
             // check if the current node matches the key node given
             // If True, stop traversing, return the element at top of queue and this is the next node in the level order traversal.
-            if (current_node === key) {
-                console.log ("key: " + key.val);
-                break;
+            if (current_node.val === key) {
+            //if (current_node === key) {
+                //console.log ("key: " + key);
+                console.log ("key: " + key);
+                if (queue[0] === null) {
+                    console.log ("queue[0] before breaking: no successor ");
+                } else {
+                    console.log( "queue[0] before breaking out of while loop: " + queue[0].val);
+                    break;
+                }
             }
         }
-        console.log( "queue[0]: " + queue[0])
+        if (queue[0] === null) {
+            console.log ("queue[0] after while : no successor ");
+        } else { 
+            console.log( "queue[0] after while loop: " + queue[0].val);
+        }
         //let result = queue[0]
-        return queue[0]
+        return queue[0];
     };
 
 var root = new TreeNode(12)
@@ -93,10 +108,23 @@ root.right = new TreeNode(1)
 root.left.left = new TreeNode(9)
 root.right.left = new TreeNode(10)
 root.right.right = new TreeNode(5)
+
+ 
+//let key = root; // node 12
+//result = find_successor(root, key)
 result = find_successor(root, 12)
 if (result != null)
     console.log(result.val)
+
+
+//let key = root.left.left; // node 9
+//result = find_successor(root, key)
 result = find_successor(root, 9)
-if (result != null)
+if (result != null) {
     console.log(result.val)
-  
+} else {
+    console.log(" No Successor")
+}
+
+// Time Complexity = O(n) The while loop will traverse N times, where N is the number of nodes in the tree.
+// Space Complexity = O(n). We are using extra space for the queue, which was used to implement level order traversal.
