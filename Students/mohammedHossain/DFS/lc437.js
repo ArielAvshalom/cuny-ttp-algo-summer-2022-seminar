3b356e2bc18e044cf42f2a4c265c0c42007a04c1
jahedev <jahedev@gmail.com>
2022-06-19 23:03:01 -0400

    lc437 stuck

--- Students/mohammedHossain/DFS/lc437.js
+++ Students/mohammedHossain/DFS/lc437.js
@@ -14,25 +14,27 @@
 // 6  5   2  3
 
 class TreeNode {
-
   constructor(value) {
     this.value = value;
     this.left = null;
     this.right = null;
   }
+}
+
+var count = 0;
+
+const count_paths = function (root, S) {
+  count_paths_total(root, S, 0);
 };
 
-
-const count_paths = function(root, S) {
-  // TODO: Write your code here
-  return -1
+const count_paths_total = function (root, S, total) {
+  return total;
 };
 
-
-var root = new TreeNode(12)
-root.left = new TreeNode(7)
-root.right = new TreeNode(1)
-root.left.left = new TreeNode(4)
-root.right.left = new TreeNode(10)
-root.right.right = new TreeNode(5)
-console.log(`Tree has paths: ${count_paths(root, 11)}`)
+var root = new TreeNode(12);
+root.left = new TreeNode(7);
+root.right = new TreeNode(1);
+root.left.left = new TreeNode(4);
+root.right.left = new TreeNode(10);
+root.right.right = new TreeNode(5);
+console.log(`Tree has paths: ${count_paths(root, 11)}`);
