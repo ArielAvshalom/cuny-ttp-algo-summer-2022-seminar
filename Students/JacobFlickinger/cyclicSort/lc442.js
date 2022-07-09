c62931e1c4f8231c084fd298db219bd36fc42425
Jacob Flickinger <69496818+jjflickinger@users.noreply.github.com>
2022-07-08 21:14:45 -0400

    added an else

--- Students/JacobFlickinger/cyclicSort/lc442.js
+++ Students/JacobFlickinger/cyclicSort/lc442.js
@@ -4,8 +4,9 @@ const find_all_duplicates = function(nums) {
     for (let i = 0; i < nums.length; i++) {
         if (set.has(nums[i])) {
             arr.push(nums[i]);
+        } else {
+            set.add(nums[i]);
         }
-        set.add(nums[i]);
     }
     return arr;
 };
