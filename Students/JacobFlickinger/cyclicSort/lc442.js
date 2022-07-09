508025707a3114ff7beb47232dc0639acb827a98
Jacob Flickinger <69496818+jjflickinger@users.noreply.github.com>
2022-07-08 21:13:47 -0400

    Create lc442.js

---
+++ Students/JacobFlickinger/cyclicSort/lc442.js
@@ -0,0 +1,11 @@
+const find_all_duplicates = function(nums) {
+    let set = new Set();
+    let arr = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (set.has(nums[i])) {
+            arr.push(nums[i]);
+        }
+        set.add(nums[i]);
+    }
+    return arr;
+};
